7398835083a6d9f6c5c0f0eee4ece703462c75e0
antonino-feitosa <antonino_feitosa@yahoo.com.br>
2022-04-07 20:48:48 -0300

    Update function.cpp

--- fibonacci/src/function.cpp
+++ fibonacci/src/function.cpp
@@ -4,6 +4,8 @@ std::vector<unsigned int> fib_below_n( unsigned int n )
 {
     // TODO: adicione o seu código aqui.
 
+//Novo comentário
+
     // TODO: Isto é apenas um STUB. Troque o retorno pelo que você julgar correto.
     return std::vector<unsigned int>{};
 }
